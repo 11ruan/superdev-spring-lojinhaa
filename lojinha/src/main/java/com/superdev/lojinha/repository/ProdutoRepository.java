4e529e807e15a319c032cee3769f1944928aa22c
Ruan lana <lanaruanpablo@gmail.com>
2024-05-17 21:38:48 -0300

    :construction: feat(produto): Implementando Repositorio para produto

---
+++ lojinha/src/main/java/com/superdev/lojinha/repository/ProdutoRepository.java
@@ -0,0 +1,75 @@
+package com.superdev.lojinha.repository;
+
+import com.superdev.lojinha.model.Produto;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProdutoRepository {
+
+    List<Produto> listaProdutos = new ArrayList<>();
+    int idSequencia = 0;
+
+    // findAll
+    public List<Produto> findAll() {
+        return this.listaProdutos;
+    }
+
+    // findBy
+    public Produto findById(int id) {
+        Produto produtoEncontrado = new Produto();
+        this.listaProdutos.forEach(produto -> {
+            if (produto.getId() == id) {
+                produtoEncontrado.setId(produto.getId());
+                produtoEncontrado.setNome(produto.getNome());
+                produtoEncontrado.setPreco(produto.getPreco());
+                produtoEncontrado.setStatus(produto.isStatus());
+            }
+        });
+        return produtoEncontrado;
+    };
+
+    // create
+    public Produto create(Produto produto) {
+        produto.setId(this.getIdSequencia());
+        this.listaProdutos.add(produto);
+        return produto;
+    }
+
+    // alter
+
+    public Produto alter(Produto produto) {
+        Produto produtoEncontrado = this.findById(produto.getId());
+
+        if (produtoEncontrado.getId() == 0) {
+            return null;
+        }
+
+        this.listaProdutos.remove(produtoEncontrado);
+        this.listaProdutos.add(produto);
+
+        return produto;
+    }
+
+    // delete
+
+    public Produto delete(int id) {
+        Produto produtoEncontrado = this.findById(id);
+
+        if (produtoEncontrado.getId() == 0) {
+            return null;
+        }
+
+        this.listaProdutos.remove(produtoEncontrado);
+        return produtoEncontrado;
+    }
+
+    private int getIdSequencia() {
+        this.idSequencia++;
+        return this.idSequencia;
+    }
+
+
+}
